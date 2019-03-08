e05265031d13e12d81498cdf196d0333fb55e981
y-moriguchi <y-moriguchi@toba-cmt.ac.jp>
2019-03-08 15:26:48 +0900

    all pattern algorithm

--- C-test.cs
+++ C-test.cs
@@ -9,38 +9,33 @@ public struct Point
     public Point(int x, int y) { this.x = x; this.y = y; }
 }
 
-public class Hello{
-    // 順列列挙用の関数
-    public static IEnumerable<T[]> Enumerate<T>(IEnumerable<T> items) {
-        if (items.Count() == 1) {
-            yield return new T[] { items.First() };
-            yield break;
-        }
-        foreach (var item in items) {
-            var leftside = new T[] { item };
-            var unused = items.Except(leftside);
-            foreach (var rightside in Enumerate(unused)) {
-                yield return leftside.Concat(rightside).ToArray();
+public class Permutation {
+    public IEnumerable<T[]> Enumerate<T>(IEnumerable<T> items) {
+        return _GetPermutations<T>(new List<T>(), items.ToList());
+    }
+
+    private IEnumerable<T[]> _GetPermutations<T>(IEnumerable<T> perm, IEnumerable<T> items) {
+        if (items.Count() == 0) {
+            yield return perm.ToArray();
+        } else {
+            foreach (var item in items) {
+                var result = _GetPermutations<T>(perm.Concat(new T[] { item }),
+                                                    items.Where(x => x.Equals(item) == false)
+                                );
+                foreach (var xs in result)
+                    yield return xs.ToArray();
             }
         }
     }
+}
 
+public class Hello{
     public static void Main(){
-        // 自分の得意な言語で
-        // Let's チャレンジ！！
-
         string line=System.Console.ReadLine();
         // お宝の総数
         int tresure_count=int.Parse(line);
-
-        // 位置と原点からの距離のリスト
-        //var pointTable=new SortedDictionary<double, Point>();
         // 位置のリスト
         var pointList=new List<Point>();
-        //IEnumerable<Point> pointList;
-        //pointList.AddRange(new Point(0,0));
-        pointList.Add(new Point(0,0));
-        //Console.WriteLine(pointList[0].x);
 
         // 位置リストを完成させる
         for (int tresure_num=0; tresure_num<tresure_count; tresure_num++){
@@ -50,7 +45,33 @@ public class Hello{
         }
 
         // N個の宝物の位置の順序の総数はN!(N*N-1...*2*1)
-        Enumerate(pointList);
-
+        var perm = new Permutation();
+        double minDistance=0;
+        var minPointList=new List<Point>();
+        foreach (var n in perm.Enumerate(pointList)) {
+            int posX=0, posY=0;
+            double distance=0;
+            foreach (var x in n) {
+                //Console.Write("{0} {1}  ", x.x, x.y);
+                distance+=(Math.Sqrt(Math.Pow(x.x-posX,2)+Math.Pow(x.y-posY,2)));
+                posX=x.x;
+                posY=x.y;
+            }
+            // 距離
+            //Console.WriteLine(distance);
+            if (minDistance==0 || minDistance>distance) {
+                minDistance=distance;
+                minPointList.Clear();
+                foreach (var x in n) {
+                    minPointList.Add(new Point(x.x,x.y));
+                }
+            }
+            // 改行
+            //Console.WriteLine();
+        }
+        //Console.WriteLine(minDistance);
+        foreach (var i in minPointList) {
+            Console.WriteLine("{0} {1}", i.x, i.y);
+        }
     }
 }
